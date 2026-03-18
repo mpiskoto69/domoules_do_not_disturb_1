3826373e77c99c73dc7ecf480c91a35331323094
mpiskoto69 <mariakouki03@gmail.com>
2026-03-18 17:41:27 +0200

    ΚΟΝΙΤΣΟΥΑ

--- tuc/bst/ExperimentRunner.java
+++ tuc/bst/ExperimentRunner.java
@@ -276,16 +276,4 @@ public class ExperimentRunner {
 
         System.out.println(row);
     }
-
-    private static class ExperimentStructures {
-        DynamicMemoryBST tree1;
-        ArrayBST tree2;
-        SortedArrayBinarySearch tree3;
-
-        ExperimentStructures(DynamicMemoryBST tree1, ArrayBST tree2, SortedArrayBinarySearch tree3) {
-            this.tree1 = tree1;
-            this.tree2 = tree2;
-            this.tree3 = tree3;
-        }
-    }
 }
